f9a5d6e17196a75ca58af83bf46265482ad4562e
Guillaume Maiano <guillaume@maiano.fr>
2021-09-13 15:38:25 +0200

    Logicash_PR_Checker init

---
+++ src/local_config.h
@@ -0,0 +1,10 @@
+#pragma once
+// check (required) parameters passed from the ini
+// create your own private_config.ini with the data. See private_config.template.ini
+#ifndef WIFI_PASSWORD
+//#error Need to pass WIFI_PASSWORD
+#endif
+
+#ifndef WIFI_SSID
+//#error Need to pass WIFI_SSID
+#endif
